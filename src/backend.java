5d6731856c5b203b047f04cbbab076452bb3ddd7
adrianghc <adrian.herrmann@fu-berlin.de>
2015-06-21 15:07:21 +0200

    backend -> Backend
    Test-Print entfernt.

--- src/backend.java
+++ src/backend.java
@@ -1,8 +1,27 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
 
-public class backend {
 
-	public static void main(String[] args) {
-		System.out.println("Test");
+public class Backend {
+	
+	private Connection conn;
+	public Backend_import backendImport;
+	public Backend_queries backendQueries;
+
+	public Backend() throws Exception {
+		
+		try {
+			Class.forName("org.postgresql.Driver");
+			String url = "jdbc:postgresql://localhost:15432/myapp?user=myapp&password=dbpass";
+			this.conn = DriverManager.getConnection(url);
+			
+			backendImport = new Backend_import(this.conn);
+			backendQueries = new Backend_queries(this.conn);
+		} 
+		catch (Exception e) {
+			throw new Exception();
+		}
+		
 	}
 	
 }
